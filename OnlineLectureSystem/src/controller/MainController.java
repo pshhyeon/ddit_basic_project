5ba5f097aacbcde169ffdb7292b5f977a075d32d
PC-07 <PC-07@DESKTOP-TOJOBBB>
2024-02-01 16:50:18 +0900

    확인해주세요~

--- OnlineLectureSystem/src/controller/MainController.java
+++ OnlineLectureSystem/src/controller/MainController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import service.UserService;
 import util.ScanUtil;
 import util.View;
-
 import vo.UserVo;
 
 // 출력부분 클래스 상속
@@ -28,7 +27,7 @@ public class MainController {
 				view = home();
 				break;
 			case LOGIN:
-				view = home();
+				view = login();
 				break;
 			default:
 				break;
@@ -37,16 +36,40 @@ public class MainController {
 	}
 	
 	private View login() {
+		System.out.println("로그인을 시작합니다");
+		String id = ScanUtil.nextLine("id : ");
+		String pass = ScanUtil.nextLine("pass : ");
+		UserVo user = (UserVo)sessionStorage.get("user");
+		MainController.sessionStorage.put("user",user);
+		List<UserVo> param = new ArrayList<UserVo>();
+		param.add(id);
+		param.add(pass);
+		UserVo userVosel= userService.login(param, (int)sessionStorage.get("login"));
+		//홈에서 sessionStorage.put해서 키 login 에 sel (1,2,3)을 넣어줌
+		//UserVo userVosel= userService.login(param, (int)sessionStorage.get("login"));
+		//여기서 userService.login에 param(id와pass)그리고 sel 값을 보내준다
+		int usersel = (int)sessionStorage.get("login");
+
 		return View.HOME;
 	}
 
 	private View home() {
-		System.out.println("1. 로그인");
+		System.out.println("1. 일반회원 로그인");
+		System.out.println("2. 강사 로그인");
+		System.out.println("3. 관리자 로그인");
+		System.out.println("4. 회원가입");
+		System.out.println("5. home");
 		int sel = ScanUtil.nextInt("메뉴 선택 : ");
+		
 		switch (sel) {
 		case 1:
-			return View.LOGIN;
 		case 2:
+		case 3:
+			sessionStorage.put("login", sel);
+			return View.LOGIN;
+		case 4:
+			return View.HOME;
+		case 5:
 			return View.HOME;
 		default:
 			return View.HOME;
--- OnlineLectureSystem/src/service/UserService.java
+++ OnlineLectureSystem/src/service/UserService.java
@@ -22,3 +22,4 @@ public class UserService {
 
 	UserDao userDao = UserDao.getInstance();
 
+}
