775c404b0467a92da5bafbb59c6213fc4ee26ebf
PC-08 <PC-08@DESKTOP-H3MP8AH>
2024-02-01 16:10:43 +0900

    시작해보자

--- OnlineLectureSystem/src/dao/UserDao.java
+++ OnlineLectureSystem/src/dao/UserDao.java
@@ -20,23 +20,5 @@ public class UserDao {
 
 	JDBCUtil jdbc = JDBCUtil.getInstance();
 
-	public UserVo login(List<Object> param) {
-		String sql = " select * from JAVA_USER where MEM_ID = ? and MEM_PASS = ? ";
 
-		return jdbc.selectOne(sql, param, UserVo.class);
-	}
-
-	public boolean signIdCheck(List<Object> param) {
-		String sql = "SELECT MEM_ID FROM JAVA_USER WHERE MEM_ID = ?";
-		if (jdbc.selectOne(sql, param) == null) {
-			return false;
-		}
-		return true;
-	}
-
-	public void sign(List<Object> param) {
-		String sql = "INSERT INTO JAVA_USER (MEM_ID, MEM_PASS, MEM_NAME)   VALUES(?, ?, ?) ";
-		jdbc.update(sql, param);
-		System.out.println("회원가입 완료");
-	}
 }
--- OnlineLectureSystem/src/service/UserService.java
+++ OnlineLectureSystem/src/service/UserService.java
@@ -22,28 +22,3 @@ public class UserService {
 
 	UserDao userDao = UserDao.getInstance();
 
-	public boolean login(List<Object> param, int mem) {
-		UserVo user = userDao.login(param);
-		if (user == null) {
-			return false;
-		}
-		
-		if(!(user.getMem_manager() == null) && mem == 1) {
-			boolean chk = true;
-			MainController.sessionStorage.put("page", View.ADMIN_HOME);
-			MainController.sessionStorage.put("admin", chk);
-		}
-		MainController.sessionStorage.put("user", user);
-		MainController.sessionStorage.remove("login");
-		return true;
-	}
-	
-	public boolean signIdCheck(List<Object> param) {
-		return userDao.signIdCheck(param);
-	}
-
-	public void sign(List<Object> param) {
-		userDao.sign(param);
-	}
-
-}
