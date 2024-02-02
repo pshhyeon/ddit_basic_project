8b1eb611d83cd87fbfed1ed950bfa153ff6cc530
PC-08 <PC-08@DESKTOP-H3MP8AH>
2024-02-02 09:29:02 +0900

    머지 다시했어

--- OnlineLectureSystem/src/dao/UserDao.java
+++ OnlineLectureSystem/src/dao/UserDao.java
@@ -27,11 +27,6 @@ public class UserDao {
 				+ " WHERE USER_ID = ? \r\n " + "   AND USER_PASS = ? \r\n "
 				+ "   AND DIVI_NO = (SELECT DIVI_NO FROM USER_DIVI WHERE DIVI_NO = " + sel + " ) ";
 
-<<<<<<< HEAD
-=======
-	     
-		
->>>>>>> refs/remotes/origin/shun
 		return jdbc.selectOne(sql, param, UserVo.class);
 	}
 
